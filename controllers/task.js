24a0a6b50de5495ea37cd8c5e73cce88e69e28f7
nyka <nykanano@gmail.com>
2023-08-28 22:59:13 +0700

    complate uodate task feature

--- controllers/task.js
+++ controllers/task.js
@@ -44,9 +44,23 @@ const deleteTask = async (req, res) =>{
   }
 }
 
+const updateTask = async (req, res) => {
+  try{
+  const {id: taskID} = req.params
+  const task = await Task.findByIdAndUpdate({_id: taskID,}, req.body,{new: true, runValidators: true})
+  if(!task){
+    res.status(500).json({msg: 'There is no task with the id: ${taskID}'})
+  }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error})
+}
+}
+
 module.exports = {
   getAllItems,
   createTask,
   getTask,
-  deleteTask
+  deleteTask,
+  updateTask
 }
--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllItems, createTask,getTask, deleteTask } = require("../controllers/task");
+const { getAllItems, createTask,getTask, deleteTask, updateTask } = require("../controllers/task");
 
 router.route('/').get(getAllItems).post(createTask)
-router.route('/:id').get(getTask).delete(deleteTask)
+router.route('/:id').get(getTask).delete(deleteTask).patch(updateTask)
 
 module.exports = router;
